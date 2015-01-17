9bb660d7774df7de1544ce7ea123e99e9cc4bc3a
Song Yang <songyang@thoughtworks.com>
2015-01-17 15:21:05 +0800

    component | use component to render

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-React.render(<img src='http://tinyurl.com/lkevsb9' />, document.body);
+// React.render(<img src='http://2.bp.blogspot.com/-SXd2K8uSRmU/T-A2QmwPxSI/AAAAAAAADWw/0vJW2l3vfMs/s200-c/javascript_logo.jpg' />, document.body);
+
+var Photo = React.createClass({
+
+  render: function() {
+    return <img src='http://2.bp.blogspot.com/-SXd2K8uSRmU/T-A2QmwPxSI/AAAAAAAADWw/0vJW2l3vfMs/s200-c/javascript_logo.jpg' />
+  }
+});
+
+React.render(<Photo />, document.body);
