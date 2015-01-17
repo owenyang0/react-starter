3538d47db176a2e8d00ec6c26f187c5551f6f117
Song Yang <songyang@thoughtworks.com>
2015-01-17 20:11:34 +0800

    state | add state control to like photo

--- app.js
+++ app.js
@@ -12,16 +12,51 @@
 // React.render(<Photo />, document.body);
 
 // Props
-var Photo = React.createClass({
+// var Photo = React.createClass({
+// 
+//   render: function() {
+//     return (
+//       <div className='photo'>
+//         <img src={this.props.imageURL} />
+//         <span>{this.props.caption}</span>
+//       </div>
+//     )
+//   }
+// });
+// 
+// React.render(<Photo imageURL='http://owenyang0.github.io/img/background.jpg' caption='Headset' />, document.body);
 
-  render: function() {
-    return (
-      <div className='photo'>
-        <img src={this.props.imageURL} />
-        <span>{this.props.caption}</span>
-      </div>
-    )
-  }
-});
-
-React.render(<Photo imageURL='http://owenyang0.github.io/img/background.jpg' caption='Headset' />, document.body);
+// State
+ var Photo = React.createClass({
+ 
+   toggleLiked: function() {
+     this.setState({
+       liked: !this.state.liked
+     });
+   },
+ 
+   getInitialState: function() {
+     return {
+       liked: false
+     }
+   },
+ 
+   render: function() {
+     var buttonClass = this.state.liked ? 'active' : '';
+ 
+     return (
+       <div className='photo'>
+         <img src={this.props.src} />
+ 
+         <div className='bar'>
+           <button onClick={this.toggleLiked} className={buttonClass}>
+             ♥
+           </button>
+           <span>{this.props.caption}</span>
+         </div>
+       </div>
+     )
+   }
+ });
+ 
+ React.render(<Photo src='http://owenyang0.github.io/img/background.jpg' caption='Headset'/>, document.body);
