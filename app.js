4d8d76bdf664f6fcbc91e12a5948c93a9d355c0d
Song Yang <songyang@thoughtworks.com>
2015-01-17 15:33:14 +0800

    props | use props to show

--- app.js
+++ app.js
@@ -1,10 +1,27 @@
-// React.render(<img src='http://2.bp.blogspot.com/-SXd2K8uSRmU/T-A2QmwPxSI/AAAAAAAADWw/0vJW2l3vfMs/s200-c/javascript_logo.jpg' />, document.body);
+// React elements
+// React.render(<img src='http://owenyang0.github.io/img/background.jpg' />, document.body);
 
+// Components
+// var Photo = React.createClass({
+// 
+//   render: function() {
+//     return <img src='http://owenyang0.github.io/img/background.jpg' />
+//   }
+// });
+// 
+// React.render(<Photo />, document.body);
+
+// Props
 var Photo = React.createClass({
 
   render: function() {
-    return <img src='http://2.bp.blogspot.com/-SXd2K8uSRmU/T-A2QmwPxSI/AAAAAAAADWw/0vJW2l3vfMs/s200-c/javascript_logo.jpg' />
+    return (
+      <div className='photo'>
+        <img src={this.props.imageURL} />
+        <span>{this.props.caption}</span>
+      </div>
+    )
   }
 });
 
-React.render(<Photo />, document.body);
+React.render(<Photo imageURL='http://owenyang0.github.io/img/background.jpg' caption='Headset' />, document.body);
